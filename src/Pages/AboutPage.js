63cdd72af2ecb78fb06a71a598193d9c8670ae83
Teory <berkay.koksall54@gmail.com>
2024-03-17 23:36:40 +0300

    Update AboutPage.js

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const AboutPage = () => {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>Welcome to the About Page</p>
+    <div id="About" style={{display:'block', marginTop: '70px'}}>
+        <div class="aboutme">
+            <h1 style={{margin:'20px'}}>Hello There I'm Berkay ^Teory^ Koksal.</h1>
+            <p style={{margin:'20px', fontSize:'14px'}}> I am a Frontend Web Developer from Turkey. I knowledge in HTML5, CSS3, JavaScript, React, Photoshop, Adobe XD and BOOTSTRAP. I have 2+ years of experience in Game development. using Unity. You can ask anything that comes to your mind.</p>
+            <a style={{margin:'20px', color:'goldenrod', fontSize:'24px', backgroundColor:'#212121', borderRadius:'15px', padding:'10px'}} href="mailto:berkay.koksall54@gmail.com">Contact Me!</a>
+        </div>
     </div>
   )
 }
