fe775a02d11138d0b378515e6e8ad5020bd217c0
Teory <berkay.koksall54@gmail.com>
2024-03-21 17:29:32 +0300

    up

--- src/Pages/SettingsPage.js
+++ src/Pages/SettingsPage.js
@@ -8,8 +8,16 @@ const SettingsPage = () => {
       {/* settings */}
       <div className="settings">
         <h2>Settings</h2>
-        <p>Settings are a set of options that can be configured to change the behavior of the application. Settings are a set of options that can be configured to change the behavior of the application. Settings are a set of options that can be configured to change the behavior of the application. Settings are a set of options that can be configured to change the behavior of the application. Settings are a set of options that can be configured to change the behavior of the application. Settings are a set of options that can be configured to change the behavior of the application. Settings are a set of options that can be configured to change the behavior of the application. Settings are a set of options that can be configured to change the behavior of the application.</p>
-        
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo autem sapiente minus et dicta earum laudantium consequatur itaque neque. Repudiandae molestiae fugit sed officia soluta explicabo dicta pariatur provident neque.
+        Nihil tempora praesentium consequuntur pariatur consectetur non voluptas odio odit adipisci, eum quo sapiente aspernatur quod vero, saepe, magnam itaque! Dolorum repellendus non unde iste cupiditate libero aperiam soluta et?
+        Aut cumque quasi expedita perferendis ab, tempore adipisci labore quibusdam saepe laboriosam quo, debitis fugit iusto quis repellat sapiente cupiditate aperiam atque reprehenderit. Nihil mollitia distinctio porro? Dicta, commodi sunt?
+        Rerum itaque ullam esse consequuntur, dolorem tenetur distinctio cupiditate hic, soluta sed amet assumenda! Nesciunt, aliquid, ducimus amet optio quae harum natus totam, quam vero ratione magnam accusamus ab dolores?
+        Rerum nisi ab voluptate nam repudiandae accusantium amet nostrum similique, expedita molestias nobis autem voluptas velit dolore eius? Vel quas quos nihil, minus inventore illo similique aut ipsam repellendus exercitationem!
+        Qui nemo voluptas rem repellat voluptates obcaecati ipsum praesentium quidem error, quae dolor cupiditate molestiae eligendi quisquam ullam aut fugiat adipisci dolore eum reprehenderit! Autem totam aliquid rerum rem facilis?
+        Illo eveniet, enim est alias error blanditiis corrupti possimus, cumque et pariatur numquam labore a at! Suscipit voluptatum voluptate maxime laudantium rerum? Illo consequuntur blanditiis cumque quam qui ad debitis!
+        Laudantium quaerat ipsam cupiditate vero in enim error optio alias velit omnis saepe aliquid ab, et tempore at vel recusandae fugiat rerum perferendis soluta tenetur eos mollitia quo nulla? Consectetur?
+        Ullam nulla laudantium quia quo? Dolorem delectus autem, perferendis ipsa minima explicabo, at nesciunt tempore dolores nemo iure officiis ad debitis fugit quia, maxime dicta et repudiandae? Numquam, voluptas ea.
+        Earum ab nesciunt consequatur velit quidem quae atque error minus provident, totam sit culpa! Unde porro fugit debitis minima a voluptates quos, nostrum cumque quaerat, tempore excepturi nobis atque molestiae.</p>
         </div>
     </div>
   )
